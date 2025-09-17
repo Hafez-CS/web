896b96f660e52e0c50870603cba00c39221e7cee
parsa-developer <parsadeveloper.py@gmail.com>
2025-09-17 19:25:01 +0330

    add: add chatroom after submitting exam with an ai answer for the first message of chatroom

---
+++ BackEnd/exam/ai_client.py
@@ -0,0 +1,6 @@
+def send_exam_to_ai(payload: dict):
+    return {
+        "feedback": "این یک پاسخ فیک از AI است",
+        "difficulty": "easy",
+        "next_step": "تمرین بیشتر"
+    }
--- BackEnd/exam/views.py
+++ BackEnd/exam/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Exam
 from .serializers import ExamSerializer
+from chat_assistant.models import Chat, ChatRoom
+from .ai_client import send_exam_to_ai
 
 QUESTIONS = [
     {"id": 1, "question": "2 + 2 ?", "options": ["3", "4", "5","6"], "answer": "4"},
@@ -35,4 +37,42 @@ class ExamSubmitView(generics.GenericAPIView):
             score=score
         )
 
-        return Response(ExamSerializer(exam).data, status=status.HTTP_201_CREATED)
+        payload = {
+            "user": request.user.email,
+            "exam_id": exam.id,
+            "title": exam.title,
+            "answers": user_answers,
+            "score": score
+        }
+
+        ai_response = send_exam_to_ai(payload)
+        ai_message = ai_response.get("feedback", "نتایج آزمون آماده است")
+
+        chat, created = Chat.objects.get_or_create(user=request.user)
+
+        room_name = f"گفتگو درباره آزمون : {exam.title}"
+        chatroom = ChatRoom.objects.create(
+            user=request.user,
+            chat=chat,
+            name=room_name
+        )
+
+        message = {
+            "room": chatroom.slug,
+            "sender": "bot",
+            "content": ai_message,
+            "exam_id": exam.id
+        }
+
+        chat.content.append(message)
+        chat.save()
+
+        return Response({
+            "exam": ExamSerializer(exam).data,
+            "ai_feedback": ai_response,
+            "chatroom": {
+                "id": chatroom.id,
+                "slug": chatroom.slug,
+                "name": chatroom.name
+            }
+        }, status=status.HTTP_201_CREATED)
